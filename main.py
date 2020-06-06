4fab9ae73a54e11b4f4789cfd5f1522c975ee251
jared2710 <u17051429@tuks.co.za>
2020-06-06 18:40:19 +0200

    Split into CNN, reading data in

--- main.py
+++ main.py
@@ -1,55 +1,50 @@
-from models.CNN import CNN
-from models.LSTM import LSTM
+#from models.CNN import CNN
+#from models.LSTM import LSTM
 
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+#print("importing tensorflow")
+#import tensorflow as tf
+#print("done importing tensorflow")
+#from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+import pandas as pd
 
-cnn = CNN()
-cnn.print()
+column_names = [    'ID','location','loc_altitude','km2','aspect','dist_motorway','dist_trunk','dist_primary',
+                    'dist_secondary','dist_tertiary','dist_unclassified','dist_residential','popn','hh',
+                    'hh_cook_charcoal','hh_cook_firewood','hh_burn_waste']
+raw_dataset = pd.read_csv("data/airqo_metadata.csv", names=column_names,
+                      na_values = "?", comment='\t',
+                      sep=",", skiprows=1)
+sensors = raw_dataset.copy()
+print(sensors)
 
-lstm = LSTM()
-lstm.print()
+column_names = [    'ID','location','temp','precip','rel_humidity',
+                    'wind_dir','wind_spd','atmos_press','target']
+raw_dataset = pd.read_csv("data/Train.csv", names=column_names,
+                      na_values = "?", comment='\t',
+                      sep=",", skiprows=1)
+dataset = raw_dataset.copy()
+print(dataset.head())
+print(dataset['rel_humidity'].head())
 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+#lstm = LSTM()
+#lstm.print()
+
+#(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+#train_images, test_images = train_images / 255.0, test_images / 255.0
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+#cnn = CNN()
+#cnn.data(train_images, train_labels, test_images, test_labels)
+#cnn.createModel()
+#cnn.trainModel()
+#cnn.testModel()
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+                    
+                    
 
-model.summary()
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
 
-model.summary()
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-history = model.fit(train_images, train_labels, epochs=10, 
-                    validation_data=(test_images, test_labels))
 
--- models/CNN.py
+++ models/CNN.py
@@ -1,4 +1,8 @@
-
+print("importing tensorflow")
+import tensorflow as tf
+print("done importing tensorflow")
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
 class CNN():
     def __init__(self):
@@ -6,3 +10,53 @@ class CNN():
     
     def print(self):
         print("CNN")
+        
+    def data(self, train_inputs, train_labels, test_inputs, test_labels):
+        self.train_inputs = train_inputs
+        self.train_labels = train_labels
+        self.test_inputs = test_inputs
+        self.test_labels = test_labels
+        
+    def createModel(self):
+        self.model = models.Sequential()
+        self.model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+        self.model.add(layers.MaxPooling2D((2, 2)))
+        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D((2, 2)))
+        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(64, activation='relu'))
+        self.model.add(layers.Dense(10))
+        self.model.summary()
+        
+    def trainModel(self):
+        self.model.compile(optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
+
+        history = self.model.fit(self.train_inputs, self.train_labels,
+        epochs=10, 
+        validation_data=(self.test_inputs, self.test_labels))
+                
+        plt.plot(history.history['accuracy'], label='accuracy')
+        plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.ylim([0.5, 1])
+        plt.legend(loc='lower right')
+        plt.show()
+
+    def testModel(self):
+        test_loss, test_acc = self.model.evaluate(self.test_inputs, self.test_labels, verbose=2)
+        print(test_loss, test_acc)
+
+
+
+
+
+
+
+
+
+
+                
